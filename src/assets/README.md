28538e53e9362d16a672697daa442d6d045e1603
tayvano <tayvano@gmail.com>
2021-12-01 13:03:40 -0800

    2021 - 1920x1080 only

---
+++ src/assets/README.md
@@ -0,0 +1,24 @@
+# Your Favicon Package
+
+This package was generated with [RealFaviconGenerator](https://realfavicongenerator.net/) [v0.16](https://realfavicongenerator.net/change_log#v0.16)
+
+## Install instructions
+
+To install this package:
+
+Extract this package in the root of your web site. If your site is <code>http://www.example.com</code>, you should be able to access a file named <code>http://www.example.com/favicon.ico</code>.
+
+Insert the following code in the `head` section of your pages:
+
+    <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png?v=2021">
+    <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png?v=2021">
+    <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png?v=2021">
+    <link rel="manifest" href="/site.webmanifest?v=2021">
+    <link rel="mask-icon" href="/safari-pinned-tab.svg?v=2021" color="#078fb2">
+    <link rel="shortcut icon" href="/favicon.ico?v=2021">
+    <meta name="apple-mobile-web-app-title" content="MyCrypto">
+    <meta name="application-name" content="MyCrypto">
+    <meta name="msapplication-TileColor" content="#7f26ca">
+    <meta name="theme-color" content="#7f26ca">
+
+*Optional* - Check your favicon with the [favicon checker](https://realfavicongenerator.net/favicon_checker)
---
+++ webpack.prod.js
@@ -0,0 +1,117 @@
+const path = require('path');
+
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
+const CleanWebpackPlugin = require('clean-webpack-plugin'); //installed via npm
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+
+const buildPath = path.resolve(__dirname, 'dist');
+
+module.exports = {
+    devtool: 'source-map',
+    entry: './src/index.js',
+    output: {
+        filename: '[name].[hash:20].js',
+        path: buildPath
+    },
+    node: {
+        fs: 'empty'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader',
+
+                options: {
+                    presets: ['env']
+                }
+            },
+            {
+                test: /\.(scss|css|sass)$/,
+                use: [
+                    {
+                        loader: MiniCssExtractPlugin.loader
+                    },
+                    {
+                        // translates CSS into CommonJS
+                        loader: 'css-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                      loader:'resolve-url-loader',
+                      options: {
+                        sourceMap: true
+                      }
+                    },                    
+                    {
+                        // Runs compiled CSS through postcss for vendor prefixing
+                        loader: 'postcss-loader',
+                        options: {
+                            sourceMap: true
+                        }
+                    },
+                    {
+                        // compiles Sass to CSS
+                        loader: 'sass-loader',
+                        options: {
+                            outputStyle: 'expanded',
+                            sourceMap: true,
+                            sourceMapContents: true
+                        }
+                    }
+                ]
+            },
+            {
+                // Load all images as base64 encoding if they are smaller than 8192 bytes
+                test: /\.(png|jpg|gif|xml|ico|webmanifest)$/,
+                use: [
+                    {
+                        loader: 'url-loader',
+                        options: {
+                            name: '[name].[hash:20].[ext]',
+                            limit: 8192
+                        }
+                    }
+                ]
+            }
+            ,
+            {
+                // Load all icons
+                test: /\.(eot|woff|woff2|svg|ttf|png|jpg|gif|svg|xml|ico|webmanifest)([\?]?.*)$/,
+                use: [
+                    {
+                        loader: 'file-loader',
+                    }
+                ]
+            }
+        ]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: './src/index.html',
+            inject: 'body',
+        }),
+        new CleanWebpackPlugin(buildPath),
+        new MiniCssExtractPlugin({
+            filename: 'styles.[contenthash].css'
+        }),
+        new OptimizeCssAssetsPlugin({
+            cssProcessor: require('cssnano'),
+            cssProcessorOptions: {
+                map: {
+                    inline: false,
+                },
+                discardComments: {
+                    removeAll: true
+                },
+                discardUnused: false
+            },
+            canPrint: true
+        })
+    ]
+};
